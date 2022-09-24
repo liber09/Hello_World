5e4697deb3244b2af3058aacc6151c6227de7551
liber09 <113246195+liber09@users.noreply.github.com>
2022-09-24 13:00:25 +0200

    Fixed the code a little

--- script.js
+++ script.js
@@ -7,20 +7,18 @@ const body = document.querySelector("body");
 Adds an eventListener to the button that listnes on 
 the event click and then runs the function below
 */
-button.addEventListener("click",function(){
-    if (headLine.textContent == "Hello World!"){
-        headLine.textContent = "GoodBye World!"
-        button.textContent = "Say Hello"
-        image.src = "cat2.gif"
-        body.style.borderColor = "yellow"
-        headLine.style.color = "yellow"
-    }
-    else{
-        headLine.textContent = "Hello World!"
-        button.textContent = "Say goodbye"
-        image.src = "cat.gif"
-        body.style.borderColor = "blue"
-        headLine.style.color = "blue"
-    }
-    
-})
+button.addEventListener("click", function () {
+  if (headLine.textContent == "Hello World!") {
+    headLine.textContent = "GoodBye World!";
+    button.textContent = "Say Hello";
+    image.src = "cat2.gif";
+    body.style.borderColor = "yellow";
+    headLine.style.color = "yellow";
+  } else {
+    headLine.textContent = "Hello World!";
+    button.textContent = "Say goodbye";
+    image.src = "cat.gif";
+    body.style.borderColor = "blue";
+    headLine.style.color = "blue";
+  }
+});
